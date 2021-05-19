c429ffe244c838e8378078c6ffb7b430573aea94
BLOSSOM1994 <46196923+BLOSSOM1994@users.noreply.github.com>
2021-05-19 18:36:19 +0430

    Update tf_util.py

--- utils/tf_util.py
+++ utils/tf_util.py
@@ -7,7 +7,8 @@ Updated by wangjksjtu
 """
 
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 def _variable_on_cpu(name, shape, initializer, use_fp16=False):
   """Helper to create a Variable stored on CPU memory.
